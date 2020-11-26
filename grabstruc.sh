551b1043d1b9a8e5f5debad090b8afed10738407
mariswow <m.cizauskas38@gmail.com>
2020-11-26 15:25:38 +0200

    Make visualization script more user friendly

--- grabstruc.sh
+++ grabstruc.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-read -e -p "Enter filename with structure contents: " filename
-grep structuredet: ${filename} | cut -c 15- > ${filename/.out/.dat}
+read -e -p "Enter filename of the simulation " filename
+mpirun meep $filename > struc.out &
+child_pid=$!
+echo $child_pid
+
+echo "Wait for simulation to initialize..."
+while kill -0 $child_pid >/dev/null 2>&1; do
+    word="$(awk '{w=$1} END{print w}' struc.out)"
+	if [[ $word == "on" ]]; then
+		kill -9 $child_pid
+	fi
+    sleep 1
+done
+
+echo "Done."
+
+grep structuredet: struc.out | cut -c 15- > struc.dat
+
+python3 visualize.py --fname ${filename/.ctl/-eps-000000.00.h5} --pname struc.dat
